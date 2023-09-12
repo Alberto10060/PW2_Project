472f42425868473922b6480c94dcad511903ee0c
Paula <47018383+Paulardz@users.noreply.github.com>
2023-09-11 22:22:37 -0600

    ReadMe y carpeta SQL

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # PW2_Project
+
+Alexis Urdiales Garza 1604065
+Alberto Fabián Chávez Leal 1855361
+Maria Paula Rodriguez Escamilla 1691594
+Pamela Sinaí Ramirez Carrizales 1742411
+
  Proyecto de programacion web 2
+
+ En este proyecto crearemos una aplicacion web que tenga de tematica la distribucion de noticias y datos variados.
+
+ Info Sphere es una página web dedicada a la creación y difusión de noticias, por medio de publicaciones donde se compartirán diferentes contenidos multimedia, ya sea imagen, video y texto. En esta se puede seguir a más usuarios para ver sus publicaciones u o noticias, de esta manera también se puede interactuar con estos comentando en sus publicaciones.
+ Esta página web tendría como inspiración a “Twitter/X” 
+
+ Dentro del repositorio se tendran primeramente 3 carpetas del proyecto:
+ 
+ -La principal de raiz estaran los HTML, CSS y PHP
+ 
+ -la de SQL estaran los scripts para los comandos y creaciond de la base de datos
+
+ -la de Resources estaran todos los materiales usables para la pagina web y el manejo de los usurios
+
---
+++ SQL/InfoSphereTabla.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE BDINFOSPHERE2023;
+USE BDINFOSPHERE2023;
+
+CREATE TABLE usuario (
+    usuario_Id INT UNIQUE AUTO_INCREMENT COMMENT 'Identificacion numerica para los usuarios',
+    usuario_Nombre VARCHAR(255) COMMENT "Nombre de Usuario",
+    usuario_ApPaterno VARCHAR(255) COMMENT "Apellido Paterno del usuario",
+    usuario_ApMaterno VARCHAR(255) COMMENT "Apellido Materno del usuario",
+    usuario_FeNac DATE COMMENT "Fecha de nacimiento del usuario",
+    usuario_Genero tinyint NOT NULL COMMENT "Genero del usuario. Se puede escoger de la lista.",
+    usuario_Email VARCHAR(255) UNIQUE COMMENT "Email del usuario",
+    usuario_Contraseña VARCHAR(255) COMMENT "Contraseña del usuario. Esta debe ser min. de 8 caracteres y con al menos una mayuscula, minuscula, numero y caracter especial",
+    usuario_Habilitado BIT DEFAULT 1 COMMENT "Indica si es activo el usuario. 0 es inactivo, 1 es activo",
+    usuario_Bloqueado BIT DEFAULT 0 COMMENT "Si esta bloqueado o no. 0 es Desbloqueado, 1 es bloqueado",
+    usuario_Imagen MEDIUMBLOB COMMENT "Imagen Tipo Avatar del usuario",
+    usuario_FeReg DATE DEFAULT (CURRENT_DATE) COMMENT "Fecha en el que se creo el usuario",
+    PRIMARY KEY (usuario_Id)
+);
+
+CREATE TABLE post (
+post_Id INT UNIQUE AUTO_INCREMENT COMMENT 'Identificacion numerica para los cursos',
+post_Nombre VARCHAR(255) COMMENT 'Nombre del curso',
+post_Descripcion VARCHAR(255) COMMENT 'Descripcion general del curso',
+post_tipo BIT COMMENT 'Dictamina si es compra completa 0 o por nivel 1',
+post_MeGusta INT NULL DEFAULT 0 COMMENT '"MeGustas" que le han dado los usuarios al post ',
+post_NoMeGusta INT NULL DEFAULT 0 COMMENT '"NoMeGustas" que le han dado los usuarios al post ', 
+post_Imagen MEDIUMBLOB COMMENT 'Imagen de portada del Curso',
+post_FeCrea DATE DEFAULT (CURRENT_DATE) COMMENT 'Fecha de creacion del curso',
+post_UsuarioId INT COMMENT 'FK Id de usuario que creo el curso',
+post_CategoriaId int COMMENT 'FK Categoria del curso',
+FOREIGN KEY (post_UsuarioId) REFERENCES usuario(Usuario_Id),
+PRIMARY KEY (post_Id)
+);
