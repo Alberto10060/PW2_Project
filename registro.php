912a0f5b552d9abf3a55885678136c739561b562
Paula <47018383+Paulardz@users.noreply.github.com>
2023-09-11 22:05:32 -0600

    creacion de phps
    se hicieron el index y registro php

---
+++ registro.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Registro.css">
+    <title>Registro</title>
+</head>
+<body> 
+    <section>
+        <div class="form-box">
+            <div class="form-value">
+                <form action="">
+                    <h2>¡Registrate!</h2>
+                    <h5>Ingresa tus datos para tener una cuenta</h5>
+                   <div class="inputbox"> <img src="Logo InfoSphere Blanco_Mesa de trabajo 1.png" alt=""></div> 
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="email" name="" id="" required>
+                        <label for="">Email</label>
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="person-circle-outline"></ion-icon>
+                        <input type="user" name="" id="" required>
+                        <label for="">Nombre de Usuario</label>
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="person-outline"></ion-icon>
+                        <input type="nombre" name="" id="" required>
+                        <label for="">Nombre</label>
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="people-outline"></ion-icon><input type="apellidos" name="" id="" required>
+                        <label for="">Apellidos</label>
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" name="" id="" required>
+                        <label for="">Password</label>
+                    </div>
+                    <div class="forget">
+                        <label for=""><input type="checkbox" name="" id="">Recordar contraseña</label>
+                        <a href="">Olvidar esta contraseña</a>
+                    </div>
+                    <button>Registrar Cuenta</button>
+                    <div class="register">
+                        <p>¿Ya tienes una cuenta? <a href="">Login</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</div>
+</div>
+</body>
+</html>
